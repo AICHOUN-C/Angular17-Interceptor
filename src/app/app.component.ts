b8b0689a2cab09627aad0b647dd12636128a23ec
AICHOUN-C <cherif.aichoun@gmail.com>
2020-12-30 11:37:49 +0100

    feat: challenge done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular17-Interceptor';
+export class AppComponent implements OnInit{
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.getTodosbyid(1);
+  }
+
+  getTodosbyid(id) {
+    this.todoService.getTodoById(id).subscribe((data) => {
+      console.log(data);
+      return data;
+    })
+  }
 }
